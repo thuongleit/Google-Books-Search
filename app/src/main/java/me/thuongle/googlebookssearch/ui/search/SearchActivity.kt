c13e0a85b9799e25e397cea335370f1e169a8dd1
Thuong Le <thuongle.it@gmail.com>
2018-11-03 18:28:06 +0200

    Enable data binding for search activity

--- app/src/main/java/me/thuongle/googlebookssearch/ui/search/SearchActivity.kt
+++ app/src/main/java/me/thuongle/googlebookssearch/ui/search/SearchActivity.kt
@@ -1,13 +1,15 @@
 package me.thuongle.googlebookssearch.ui.search
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import me.thuongle.googlebookssearch.R
+import me.thuongle.googlebookssearch.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        val binding = DataBindingUtil.setContentView<ActivitySearchBinding>(this, R.layout.activity_search)
     }
 }
