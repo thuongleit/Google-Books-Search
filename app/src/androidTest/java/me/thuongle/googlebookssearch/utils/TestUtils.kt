c9e27a44fd0f00b1fd53c2f37679b28533ec6acc
Thuong Le <thuongle.it@gmail.com>
2018-11-03 10:22:36 +0200

    Improve main screen and add UI tests to check default

---
+++ app/src/androidTest/java/me/thuongle/googlebookssearch/utils/TestUtils.kt
@@ -0,0 +1,44 @@
+package me.thuongle.googlebookssearch.utils
+
+import android.support.test.espresso.matcher.BoundedMatcher
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+
+fun hasItemCount(itemCount: Int): Matcher<View> {
+    return object : BoundedMatcher<View, RecyclerView>(
+        RecyclerView::class.java
+    ) {
+
+        override fun describeTo(description: Description) {
+            description.appendText("has $itemCount items")
+        }
+
+        override fun matchesSafely(view: RecyclerView): Boolean {
+            return if (view.adapter == null) {
+                 itemCount == 0
+            } else return view.adapter!!.itemCount == itemCount
+        }
+    }
+}
+
+private fun childAtPosition(
+    parentMatcher: Matcher<View>, position: Int
+): Matcher<View> {
+
+    return object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description) {
+            description.appendText("Child at position $position in parent ")
+            parentMatcher.describeTo(description)
+        }
+
+        public override fun matchesSafely(view: View): Boolean {
+            val parent = view.parent
+            return parent is ViewGroup && parentMatcher.matches(parent)
+                    && view == parent.getChildAt(position)
+        }
+    }
+}
